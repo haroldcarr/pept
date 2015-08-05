ebe10358c615a89b9898ab84e08e2cc555b38004
Harold Carr <harold.carr@gmail.com>
2015-08-05 16:20:24 -0700

    Sketch of initiator (aka 'client') PEPT.

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,12 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main =
+    let c = ContactInfo { address       = "localhost"
+                        , msgDispatcher = MsgDispatcher { sendMD = \x -> putStrLn (body x) }
+                        , connection    = Connection    { sendC  = \x -> putStrLn (body x) }
+                        , encoder       = Encoder { encodeAndSend = \mi conn -> (sendC conn) mi }
+                        }
+        d = createDelegate [c]
+        m = MsgInfo { body = "my message body" }
+    in (sendD d) m
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,52 @@
 module Lib
-    ( someFunc
-    ) where
+(
+  Connection(..)
+, ContactInfo(..)
+, Delegate(..)
+, Encoder(..)
+, MsgDispatcher(..)
+, MsgInfo(..)
+, createDelegate
+)
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data Delegate = Delegate {
+    contactInfoList :: [ContactInfo]
+  , sendD           :: MsgInfo -> IO ()
+}
+
+data MsgInfo = MsgInfo {
+    body :: String
+}
+
+data ContactInfo = ContactInfo {
+    address       :: String
+  , msgDispatcher :: MsgDispatcher
+  , connection    :: Connection
+  , encoder       :: Encoder
+}
+
+data MsgDispatcher = MsgDispatcher {
+    sendMD :: MsgInfo -> IO ()
+}
+
+data Encoder = Encoder {
+    encodeAndSend :: MsgInfo -> Connection -> IO ()
+}
+
+data Connection = Connection {
+    sendC :: MsgInfo -> IO ()
+}
+
+createDelegate :: [ContactInfo] -> Delegate
+createDelegate contactInfo =
+  Delegate { contactInfoList = contactInfo
+           , sendD           = delegateSend contactInfo
+           }
+
+delegateSend :: [ContactInfo] -> MsgInfo -> IO ()
+delegateSend contactInfoList msgInfo = do
+    let ci = head contactInfoList
+        md = msgDispatcher ci
+    putStrLn (address ci)
+    putStrLn (body msgInfo)
